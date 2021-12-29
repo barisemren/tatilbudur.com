c9553e1a97152c42f426f8778708da8c874d61b0
Barış Emren <baris.emren@yahoo.com>
2021-12-29 18:01:52 +0300

    Update mobile-app.js

--- mobile-app.js
+++ mobile-app.js
@@ -18,3 +18,35 @@ function menuClose() {
     menu.style.width ="0px";
     menu.style.transition = "width 0.5s";
 }
+
+function hotelsOpen() {
+    const hotels = document.querySelector(".mobile-menu-hotels-open");
+    
+    
+    if(hotels.style.height == "0px"){
+        hotels.style.height = "389px";
+        hotels.style.transition = "height 1s"
+        hotels.style.margin= "10px 40px 20px 23px";
+    }
+    else{
+        hotels.style.height = "0px";
+        hotels.style.transition = "height 1s, margin 1s";
+        hotels.style.margin= "0px 40px 0px 23px";
+    }
+}
+
+function toursOpen() {
+    const tours = document.querySelector(".mobile-menu-tours-open");
+    
+    
+    if(tours.style.height == "0px"){
+        tours.style.height = "289px";
+        tours.style.transition = "height 1s"
+        tours.style.margin= "10px 40px 20px 23px";
+    }
+    else{
+        tours.style.height = "0px";
+        tours.style.transition = "height 1s, margin 1s";
+        tours.style.margin= "0px 40px 0px 23px";
+    }
+}
